8f608c328d2cccc48033efa2794f6fa4cba4c2ee
jameszachman2023 <james.d.zachman@gmail.com>
2021-02-15 22:45:30 -0600

    Car game test

---
+++ Assets/Scripts/BlueCarControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueCarControl : MonoBehaviour
+{
+    public Rigidbody2D BackWheel, FrontWheel;
+    public float power;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("d"))
+        {
+            BackWheel.AddTorque(-power);
+            FrontWheel.AddTorque(-power);
+        }
+    }
+}
